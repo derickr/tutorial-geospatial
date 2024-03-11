8c13f82ffefb804b9214e64d96a0e2d1ed572a49
Derick Rethans <github@derickrethans.nl>
2024-03-11 16:13:53 +0000

    Update property in constructor

--- geojson/linestring.c
+++ geojson/linestring.c
@@ -10,4 +10,6 @@ ZEND_METHOD(Geospatial_GeoJSON_LineString, __construct)
 	ZEND_PARSE_PARAMETERS_START(1,1)
 		Z_PARAM_ARRAY(points)
 	ZEND_PARSE_PARAMETERS_END();
+
+	zend_update_property(Z_OBJCE_P(ZEND_THIS), Z_OBJ_P(ZEND_THIS), "points", strlen("points"), points);
 }
